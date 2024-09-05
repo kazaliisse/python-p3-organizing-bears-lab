bdf103be98d37f2a238ce5dcb2f3447e59b0507f
noor <noorhafowbare@gmail.com>
2024-09-05 16:52:49 +0300

    initial commit

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+  id INTEGER,
+  name	TEXT,
+  age	TEXT,
+  sex	TEXT,
+  color	TEXT,
+  temperament	TEXT,
+  alive	BOOLEAN
+);
---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', TRUE),
+('Rowdy', 8, 'F', 'Green', 'UnFriendly', FALSE),
+('Tabitha', 7, 'F', 'Blue', 'Friendly', TRUE),
+('Sergeant Brown', 4, 'M', 'Brown', 'Friendly', FALSE),
+('Melissa', 9, 'F', 'Black', 'lovely', FALSE),
+('Grinch', 2, 'F', 'Blue', 'Friendly', TRUE),
+('Wendy', 3, 'M', 'Brown', 'Friendly', FALSE),
+(NULL, 6, 'F', 'Black', 'lovely', FALSE)
